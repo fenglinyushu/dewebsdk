c483d08d3828fdb09f20ad9852a81123f94f6428
fenglinyushu <fenglinyushu@163.com>
2021-04-14 11:05:27 +0800

    增加了纯数据交互功能。demo见dwDirect https://delphibbs.com/{"m":"direct","n":"dwdirect","data":"abc"}

---
+++ source/demos/dwdirect/dwdirect.dpr
@@ -0,0 +1,27 @@
+﻿library dwdirect;
+
+uses
+  SysUtils,
+  Forms,
+  Messages,
+  StdCtrls,
+  Variants,
+  Windows,
+  Classes;
+
+{$R *.res}
+
+
+
+function dwDirectInteraction(AData:PWideChar):PWideChar;stdcall;
+begin
+     Result    := PWideChar('Direct DeWeb Interaction  '+ IntToStr(GetTickCount));
+end;
+
+
+
+
+exports
+     dwDirectInteraction;
+
+end.
