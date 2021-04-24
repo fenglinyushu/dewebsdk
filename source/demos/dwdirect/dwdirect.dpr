7e8845817389a81ccb5117fbb6a9537bf3b85b37
fenglinyushu <fenglinyushu@163.com>
2021-04-24 16:24:46 +0800

    增加了对通过post/get进行纯数据交互的支持。例程：source\demos\dwdirect.此功能支持采用其他非Delphi工具进行开发

--- source/demos/dwdirect/dwdirect.dpr
+++ source/demos/dwdirect/dwdirect.dpr
@@ -12,10 +12,25 @@ uses
 {$R *.res}
 
 
-
 function dwDirectInteraction(AData:PWideChar):PWideChar;stdcall;
+     function _Get(AParaName:String):String;
+     var
+          iPos      : Integer;     //位置,类似:name=
+     begin
+          Result    := AData;
+          //删除以前的
+          iPos      := Pos(AParaName+'=',Result);
+          Delete(Result,1,iPos+Length(AParaName));
+          //删除&后面的内容
+          iPos      := Pos('&',Result);
+          if iPos>0 then begin
+               Result    := Copy(Result,1,iPos-1);
+          end;
+     end;
 begin
-     Result    := PWideChar('Direct DeWeb Interaction  '+ IntToStr(GetTickCount));
+     //name%3Dww%26age%3D18
+
+     Result    := PWideChar(Format('You Name is %s Age is %s',[_Get('name'),_Get('age')]));
 end;
 
 
